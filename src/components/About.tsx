fef81b0e91ae5b0963a4787c318662900b5123ef
João Bartolot <jvcbartolot@gmail.com>
2025-02-12 01:04:14 -0300

    Made about responsive

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,21 +7,21 @@ const About = () => {
 			className="flex flex-col justify-center items-center w-full py-16"
 		>
 			<div className="font-display text-2xl">About_</div>
-			<div className="flex flex-col md:flex-row my-24 max-h-80 space-x-16">
-				<div className="border-4 max-h-80 max-w-80 aspect-square border-secondary rounded-md overflow-hidden">
+			<div className="flex flex-col md:flex-row justify-center items-center my-12 space-y-4 md:space-x-16 max-w-60 md:max-w-max">
+				<div className="border-4 max-w-60 md:max-w-80 aspect-square border-secondary rounded-2xl overflow-hidden">
 					<img
 						src="about.webp"
 						alt="Me"
 						className="w-full h-full object-cover"
 					/>
 				</div>
-				<div className="flex flex-col justify-between text-start">
-					<div className="text-3xl font-bold">
+				<div className="flex flex-col justify-between items-center md:items-start text-center md:text-start space-y-4 md:space-y-0">
+					<div className="text-2xl md:text-3xl font-bold">
 						HI THERE,
 						<br />
 						I’M JOÃO BARTOLOT
 					</div>
-					<div className="font-light w-full text-3xl text-secondary">
+					<div className="font-light w-full text-2xl md:text-3xl text-secondary">
 						A full-stack developer
 					</div>
 					<div className="max-w-xs">

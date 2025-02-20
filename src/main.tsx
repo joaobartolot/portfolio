048ed1856b3b3a5c2e7a5a43e6b085014a32e3c5
João Bartolot <jvcbartolot@gmail.com>
2025-02-19 21:50:05 -0300

    Remove @vercel/analytics dependency and related code from package.json and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
-import { inject } from '@vercel/analytics';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
-if (import.meta.env.PROD) {
-	inject();
-}
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
